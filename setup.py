53d9779d1bb9c8cb2e1c9db3004cbe856df96a89
Owais Lone <loneowais@gmail.com>
2011-10-08 14:38:22 +0530

    Added README.rst and changed icons

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 import os
-import site
 from distutils.core import setup
 
 from DistUtilsExtra.auto import *
@@ -12,22 +11,18 @@ for folder in os.walk(os.path.join(os.curdir, data_prefix)):
     for file in folder[-1]:
         data_files.append(os.path.join(base_folder, file))
 
-site.addsitedir('/opt/owaislone/')
 
 setup(
     name='gmailwatcher',
-    version='11.10',
+    version='11.10.3',
     author='Owais Lone',
     author_email='hello@owaislone.org',
     scripts=['bin/gmailwatcher'],
     packages=['gmailwatcher', 'gmailwatcher.lib', 'gmailwatcher.app'],
     package_data = {'gmailwatcher': data_files},
-    #data_files=[('/usr/share/indicators/messages/applications', ['%sindicators/gmailwatcher' % data_prefix])],
     cmdclass={
         'build': build_extra.build_extra,
-        #'build_icons': build_icons.build_icons,
-        #'build_i18n': build_i18n.build_i18n,
         },
     license='GNU GPLv3',
-    long_description='long long long description',
+    long_description='A gmail and google apps mail notifier with instant notifications, multiple accounts and summary view.'
 )
